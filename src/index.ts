74c51c83e5dde021d877d947f282e1a75da32b86
Ido <gershido@gmail.com>
2023-12-04 11:53:37 +0200

    cleanup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { HatsSignerGateClient } from "client";
+import {
+  HATS_SIGNER_GATE_FACTORY_ABI,
+  HATS_SIGNER_GATE_ABI,
+  MULTI_HATS_SIGNER_GATE_ABI,
+} from "./constants";
+
+export {
+  HatsSignerGateClient,
+  HATS_SIGNER_GATE_FACTORY_ABI,
+  HATS_SIGNER_GATE_ABI,
+  MULTI_HATS_SIGNER_GATE_ABI,
+};
